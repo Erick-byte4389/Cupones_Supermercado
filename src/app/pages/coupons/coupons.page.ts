f5f620a4aafd9f1f57d27e6d8ad1c9215490892f
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-25 13:18:41 -0600

    Añadiendo escaner

--- src/app/pages/coupons/coupons.page.ts
+++ src/app/pages/coupons/coupons.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerScanResult } from '@capacitor/barcode-scanner';
 import { NavController, NavParams } from '@ionic/angular';
 import { Coupon } from 'src/app/models/coupon';
 import { CouponsService } from 'src/services/coupons.service';
+import { ToastService } from 'src/services/toast.service';
 
 @Component({
   selector: 'app-coupons',
@@ -16,7 +18,8 @@ export class CouponsPage implements OnInit {
   constructor(
     private couponsService: CouponsService,
     private navParams: NavParams,
-    private navController: NavController
+    private navController: NavController,
+    private toastService: ToastService
   ) { 
     this.coupons = [];
     this.couponsActive = false;
@@ -39,6 +42,36 @@ export class CouponsPage implements OnInit {
   goToCard() {
     this.navParams.data["coupons"] = this.coupons.filter(c => c.active);
     this.navController.navigateForward('card-coupon');
+  }
+
+  startCamera() {
+
+    CapacitorBarcodeScanner.scanBarcode({
+      hint: 0
+    }).then( (value: CapacitorBarcodeScannerScanResult) => {
+      const result = value.ScanResult;
+
+      try {
+        let coupon: Coupon = JSON.parse(result);
+
+        if (this.isCouponValid(coupon)) {
+          this.toastService.showToast('QR escaneado con éxito')
+          this.coupons.push(coupon);
+        } else {
+          this.toastService.showToast('QR invalido')
+        }
+      } catch (error) {
+        this.toastService.showToast('QR error')
+        console.error(error);
+        
+      }
+      
+    })
+
+  }
+
+  private isCouponValid(coupon: Coupon) {
+    return coupon && coupon.id_Product && coupon.img && coupon.name && coupon.discount;
   }
 
 }
