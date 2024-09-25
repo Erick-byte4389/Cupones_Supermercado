3a46d6732f51f132a1765242ade1ce1250fa03fe
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-25 10:05:10 -0600

    Configuracion inicial del proyecto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '05_coupons_app',
+  appName: 'Cupones',
   webDir: 'www'
 };
 
