be9293746fb530b3788d608528f8980f9c72ecbe
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-25 12:07:20 -0600

    Enlace a nuestra tarjeta con ion-fab-button

--- src/app/pages/coupons/coupons.page.ts
+++ src/app/pages/coupons/coupons.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
 import { Coupon } from 'src/app/models/coupon';
 import { CouponsService } from 'src/services/coupons.service';
 
@@ -12,7 +13,9 @@ export class CouponsPage implements OnInit {
   public coupons: Coupon[];
 
   constructor(
-    private couponsService: CouponsService
+    private couponsService: CouponsService,
+    private navParams: NavParams,
+    private navController: NavController
   ) { 
     this.coupons = [];
   }
@@ -27,6 +30,11 @@ export class CouponsPage implements OnInit {
 
   changeActive(coupon: Coupon) {
     coupon.active = !coupon.active;
+  }
+
+  goToCard() {
+    this.navParams.data["coupons"] = this.coupons.filter(c => c.active);
+    this.navController.navigateForward('card-coupon');
   }
 
 }
